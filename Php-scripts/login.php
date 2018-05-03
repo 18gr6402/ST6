c68cf13066d2678732f0a7d941eb7fc72e0edf08
Mette Tøttrup Gade <mtg15@student.aau.dk>
2018-05-03 14:36:43 +0200

    Kald til android studio kodens brugernavn og adgnagskode er ændret

--- Php-scripts/login.php
+++ Php-scripts/login.php
@@ -1,7 +1,7 @@
 <?php 
 require "conn.php";
-$cpr_id = "1234567890";
-$user_pass = "steffenkode";
+$cpr_id = $_POST["user_name"];
+$user_pass = $_POST["password"];
 $mysql_qry = "select * from patient where cpr like '$cpr_id' and password like '$user_pass';";
 $result = mysqli_query($conn ,$mysql_qry);
 if(mysqli_num_rows($result) > 0) {
